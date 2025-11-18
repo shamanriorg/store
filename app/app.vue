462d04c769c4aacfdadf75e05e58a028ab9b8ce8
Igor Matveev <i.matveev.v@gmail.com>
2025-11-18 11:28:27 +0300

    Added analytics

--- app/app.vue
+++ app/app.vue
@@ -45,6 +45,38 @@ useHead({
       content:
         'Студия Shamanri создаёт авторские паттерны, плитку, открытки и цифровые коллекции, вдохновлённые природой и сезонной магией.'
     }
+  ],
+  script: [
+    // Google Analytics 4
+    {
+      src: 'https://www.googletagmanager.com/gtag/js?id=G-VC8CSHGSH0',
+      async: true
+    },
+    {
+      innerHTML: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-VC8CSHGSH0');
+      `
+    },
+    // Яндекс.Метрика
+    {
+      type: 'text/javascript',
+      innerHTML: `
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+        ym(88167231, "init", {
+          clickmap:true,
+          trackLinks:true,
+          accurateTrackBounce:true,
+          webvisor:true
+        });
+      `
+    }
   ]
 })
 </script>
